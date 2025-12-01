a54b75dc131b4aede00a5016b09d3ab52bfa95cc
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-12-01 21:10:04 +0530

    swaping the number with different methods.

---
+++ swap.js
@@ -0,0 +1,24 @@
+let a=10
+let b=24
+let c;
+
+// c=a
+// a=b
+// b=c
+// console.log(a);
+// console.log(b);
+
+
+//  second method
+
+// a=a+b
+// b=a-b
+// a=a-b
+// console.log(a);
+// console.log(b);
+
+// third method
+
+[a,b]=[b,a]
+console.log(a);
+console.log(b);

fda4e92f122d722d5bfde410a9edc11b71d839f5
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-12-03 18:34:00 +0530

    increment decrement

--- increment.js
+++ increment.js
@@ -1,10 +1,19 @@
-let a=10
-i=a++ + ++a;
-//  10 + 12
-console.log(i);
+// let a=10
+// i=a++ + ++a;
+// //  10 + 12
+// console.log(i);
 
-let b=11,c=13
-let d=a + a++ + ++a + b + b++ + ++b + c
+// let b=11,c=13
+// d= (a + a++ + ++a + b + b++ + ++b + c)
+// // 12 + 12 +14 + 11 + 11 + 13 + 13 
+// //   86
+// console.log(d);
 
-//   86
-console.log(d);
+let a = true;
+let b = false;
+a++
+b++
+console.log(a);
+console.log(b);
+
+

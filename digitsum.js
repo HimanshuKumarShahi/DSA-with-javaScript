64f748cda516022c7453e679edf39cc462f0d617
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-12-04 20:00:58 +0530

    find out maximum and minimum no's.

--- digitsum.js
+++ digitsum.js
@@ -7,3 +7,10 @@ console.log(a%1000);
 
 
 console.log(Math.floor(a/10));
+
+
+// find greatest one 
+let b=32,c=44444
+
+console.log("Maximum number:",Math.max(a,b,c));
+console.log("Minimum number:",Math.min(a,b,c));

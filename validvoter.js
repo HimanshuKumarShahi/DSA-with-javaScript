4859f76230c39f8b5d87c3eae4d50ac74027b16a
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-12-07 19:53:03 +0530

    conditional check.

---
+++ validvoter.js
@@ -0,0 +1,14 @@
+//  let age="rakesh"
+ let age=18
+
+ if(isNaN(age)){
+    console.log("Please enter valid input");
+ }
+
+else if(age>=18){
+    console.log("You are eligible for voting.");
+ }
+ else{
+    console.log("--Not eligible for voting --");
+    
+ }

9facb51609b01429c163080ca82827a575387e3d
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-08-27 18:44:35 +0530

    three swap method learn

--- swap.js
+++ swap.js
@@ -2,20 +2,20 @@ let a=10
 let b=24
 let c;
 
-// c=a
-// a=b
-// b=c
-// console.log(a);
-// console.log(b);
+c=a
+a=b
+b=c
+console.log(a);
+console.log(b);
 
 
 //  second method
 
-// a=a+b
-// b=a-b
-// a=a-b
-// console.log(a);
-// console.log(b);
+a=a+b
+b=a-b
+a=a-b
+console.log(a);
+console.log(b);
 
 // third method
 
